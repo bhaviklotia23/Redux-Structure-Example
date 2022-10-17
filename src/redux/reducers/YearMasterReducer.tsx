70915ada6ab2a8aff1a1558a1c5bad2e9bf48326
blotia-stacksum <bhavikl.stacksum@gmail.com>
2022-10-17 16:16:27 +0530

    Update : Status

--- src/redux/reducers/YearMasterReducer.tsx
+++ src/redux/reducers/YearMasterReducer.tsx
@@ -1,18 +1,18 @@
-import { YearMasterListConstant } from "../constants";
+// import { YearMasterListConstant } from "../constants";
 
-const initialState = {
-  yearData: [],
-};
+// const initialState = {
+//   yearData: [],
+// };
 
-export default function YearMasterReducer(
-  state = initialState || null,
-  action: any
-) {
-  switch (action.type) {
-    case YearMasterListConstant.GET_YEAR_DATA_SUCCESS:
-      return { ...state, yearData: action?.payload?.data };
-    default:
-      return state;
-  }
-}
+// export default function YearMasterReducer(
+//   state = initialState || null,
+//   action: any
+// ) {
+//   switch (action.type) {
+//     case YearMasterListConstant.GET_YEAR_DATA_SUCCESS:
+//       return { ...state, yearData: action?.payload?.data };
+//     default:
+//       return state;
+//   }
+// }
 
--- utils/AdminService.tsx
+++ utils/AdminService.tsx
@@ -1,44 +1,44 @@
-import axios from "axios";
+// import axios from "axios";
 
-// export const API_URL = " http://143.244.170.168:7043/";
-export const API_URL = "http://192.168.1.15:7043";
-// export const API_URL = process.env.REACT_APP_API_URL_GENER;
+// // export const API_URL = " http://143.244.170.168:7043/";
+// export const API_URL = "http://192.168.1.15:7043";
+// // export const API_URL = process.env.REACT_APP_API_URL_GENER;
 
-interface AuthModel {
-  accessToken: string;
-}
+// interface AuthModel {
+//   accessToken: string;
+// }
 
-const getAuth = (): AuthModel | undefined => {
-  const auth: AuthModel = {
-    accessToken: "",
-  } as AuthModel;
-  return auth;
-};
+// const getAuth = (): AuthModel | undefined => {
+//   const auth: AuthModel = {
+//     accessToken: "",
+//   } as AuthModel;
+//   return auth;
+// };
 
-export const  apiAuth = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// export const  apiAuth = axios.create({
+//   baseURL: API_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-const setupAxios = (axios: any) => {
-  axios.defaults.headers.Accept = "application/json";
-  axios.interceptors.request.use(
-    (config: any) => {
-      const auth = getAuth();
-      if (auth) {
-        config.headers.Authorization = `Bearer ${auth.accessToken}`;
-      }
-      if (config?.urlParams && typeof config?.url?.replace === "function") {
-        Object.entries(config.urlParams || {}).forEach(([key, value]) => {
-          config.url = config.url.replace(`:${key}`, value); // encodeURIComponent()
-        });
-      }
-      return config;
-    },
-    (err: any) => Promise.reject(err)
-  );
-};
+// const setupAxios = (axios: any) => {
+//   axios.defaults.headers.Accept = "application/json";
+//   axios.interceptors.request.use(
+//     (config: any) => {
+//       const auth = getAuth();
+//       if (auth) {
+//         config.headers.Authorization = `Bearer ${auth.accessToken}`;
+//       }
+//       if (config?.urlParams && typeof config?.url?.replace === "function") {
+//         Object.entries(config.urlParams || {}).forEach(([key, value]) => {
+//           config.url = config.url.replace(`:${key}`, value); // encodeURIComponent()
+//         });
+//       }
+//       return config;
+//     },
+//     (err: any) => Promise.reject(err)
+//   );
+// };
 
-setupAxios(apiAuth);
+// setupAxios(apiAuth);
